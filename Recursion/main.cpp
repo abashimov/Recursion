09b93061d162c166c909fb061519643a38000af3
Atabay Bashimov <migooface27@gmail.com>
2024-03-29 12:39:43 +0100

    Initial Commit

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,14 +1,30 @@
 //
 //  main.cpp
-//  Recursion
+//  Recursion - File System Traversal
 //
 //  Created by Atabay on 2024/3/28.
 //
 
 #include <iostream>
+#include <filesystem>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+namespace fs = filesystem;
+
+void list_files(fs::path path){
+    if (fs::is_regular_file(path)) {
+        cout<<path<<endl;
+    }else if(fs::is_directory(path)){
+        cout<<path<<":"<<endl;
+        for(auto& entry : fs::directory_iterator(path)){
+            list_files(entry.path());
+        }
+    }
+}
+
+int main() {
+    fs::path path = "/Users/name/Desktop"; // insert path here
+    list_files(path);
+    
     return 0;
 }
